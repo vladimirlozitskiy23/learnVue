8a02ccdeeb63cb94c19b405cb0ce921f524a5096
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-03-01 18:15:23 +0300

    Init app

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="App">
+    <!-- //можно писать такой тэг, todos-list придумываем сами можно написать что угодно
+    //если состоит из двух слов то в атрибутах используем кебаб кейс
+    // v-bind: нужно использовать чтобы свойство todos передалось как массив, если
+    //этого не сделать то передастся просто строка -->
+    <TodoList :todos="todos" @delete-todo="deleteTodo"/>
+  </div>
+
+</template>
+
+<script>
+
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  // data должна быть в виде функции и возвращать наши свойства
+  data() {
+    return {
+        todos: ['ремонт','завтрак', 'обед']
+
+    }
+  },
+  // регистрируем
+  components: {
+    TodoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+<ul>
+    <!-- здесь обязательно использовать key, чтобы использовать и перемещать элементы
+    если бы мы получали данные из базы данных то использовали бы поле id -->
+    <li v-for="(todo,index) in todos" @click="deleteTodo(index)" :key="index">{{ todo }}</li>
+</ul>
+</template>
+
+<script>
+    export default {
+        //чтобы получить данные используем свойство props,если бы мы использовали в App.vue кебаб кейс
+        // todos-list например в атрибутах,то здесь нужно было бы писать в кэмэл кейс ["todosList"]
+        props: ['todos'],
+        methods: {
+            deleteTodo(index){
+                this.$emit("delete-todo",index);
+            }
+        }
+    }  
+
+</script>
